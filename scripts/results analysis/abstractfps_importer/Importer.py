dfad933f9e9756167f145304b147af7f600cc8ba
Ben Boudaoud <bboudaoud@nvidia.com>
2019-04-19 12:13:44 -0400

    Updated importer for db files to be more generic

--- scripts/results analysis/abstractfps_importer/Importer.py
+++ scripts/results analysis/abstractfps_importer/Importer.py
@@ -11,41 +11,70 @@ class Trial:
         self.taskExecTime = taskExecTime
         self.success = bool(success)
 
+class Event:
+    def __init__(self, time, eventType):
+        self.time = time
+        self.type = eventType
+
 class Importer:
+    """Simple class for importing data from abstract-fps results files"""
     IN_LOG_TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
     def __init__(self, dbName):
         self.db = sqlite3.connect(dbName)
 
-    def getTableRows(self, tableName):
+    def queryDb(self, query):
+        """Simple method to query the db"""
         c = self.db.cursor()
-        c.execute('SELECT * FROM {0}'.format(tableName))
+        c.execute(query)
         return c.fetchall()
 
+    def getTableRows(self, tableName):
+        """Get all rows from a particular table"""
+        return self.queryDb('SELECT * FROM {0}'.format(tableName))
+
     def getTrials(self):
-        c = self.db.cursor()
-        c.execute('SELECT * FROM Trials')
-        rows = c.fetchall()
+        """Get all trials from the trials table"""
         trials = []
-        for row in rows:
+        for row in self.getTableRows('Trials'):
             trials.append(Trial(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
         return trials
 
+    def getTrialIds(self):
+        """Get a list of trial ids from the trials table"""
+        ids = []
+        for trial in self.getTrials(): ids.append(trial.id)
+        return ids
+
     def getTrialById(self, condId):
-        trials = self.getTrials()
-        for trial in trials: 
-            if trial.id == condId: return trial
+        """Get a particular trial(s) from the trials table"""
+        rows = self.queryDb('SELECT * FROM Trials WHERE [condition_ID] = {0}'.format(condId))
+        if(len(rows) > 1):
+            out = []
+            for row in rows: out.append(Trial(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+            return out
+        elif(len(rows) == 1): 
+            row = rows[0]
+            return Trial(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
         return None
 
+    def getEvents(self):
+        """Get all events from the events table"""
+        rows = self.getTableRows('event_log')
+        events = []
+        for row in rows: events.append(Event(row[0], row[1]))
+        return events
+
     def getTargetPositionsXYZ(self, condId):
+        """Get all target positions (for a given condition id) from the Target_Trajectory table"""
         trial = self.getTrialById(condId)
+        rows = self.queryDb("SELECT * FROM Target_Trajectory WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'")
         positions = []
-        rows = self.db.cursor().execute("SELECT * FROM Target_Trajectory WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'")
-        for row in rows:
-            positions.append([row[1], row[2], row[3]])
+        for row in rows: positions.append([row[1], row[2], row[3]])
         return positions
 
     def getTargetPositionsAzimElev(self, condId):
+        """Get all target positions (for a given condition id) as azim/elev from Target_Trajectory table"""
         positionsXYZ = self.getTargetPositionsXYZ(condId)
         positions = []
         for [x,y,z] in positionsXYZ:
@@ -56,9 +85,9 @@ class Importer:
         return positions
     
     def getPlayerActions(self, condId):
+        """Get player actions (for a given condition id) from the Player_Action table"""
         trial = self.getTrialById(condId)
+        rows = self.queryDb("SELECT * FROM Player_Action WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'")
         actions = []
-        rows = self.db.cursor().execute("SELECT * FROM Player_Action WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'")
-        for row in rows:
-            actions.append([row[2], row[3], row[1]])
+        for row in rows: actions.append([row[2], row[3], row[1]])
         return actions
