121654b9f5fdd5a2973ba69ca4ed5cfdfe44f081
Ben Boudaoud <bboudaoud@nvidia.com>
2019-10-15 17:24:47 -0400

    Updating the python importer for db changes

--- scripts/results analysis/abstractfps_importer/Importer.py
+++ scripts/results analysis/abstractfps_importer/Importer.py
@@ -1,5 +1,9 @@
 import sqlite3
 import math
+from datetime import datetime
+
+IN_LOG_TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+
 class Trial:
     def __init__(self, conditionId, sessName, sessMode, startTime, endTime, taskExecTime, success, index=-1):
         self.id = conditionId
@@ -10,6 +14,31 @@ class Trial:
         self.endTime = endTime
         self.taskExecTime = taskExecTime
         self.success = bool(success)
+
+class PlayerAction:
+    def __init__(self, t, pos_az, pos_el, pos_x, pos_y, pos_z, event, targetId=None):
+        self.time = datetime.strptime(t, IN_LOG_TIME_FORMAT)
+        self.view_az = float(pos_az)
+        self.view_el = float(pos_el)
+        self.pos_x = float(pos_x)
+        self.pos_y = float(pos_y)
+        self.pos_z = float(pos_z)
+        self.event = event
+        self.targetId = targetId
+
+class Target:
+    def __init__(self, targetId, trialId, tType, tParams, model):
+        self.id = targetId
+        self.trial = trialId
+        self.type = tType
+        self.params = tParams
+        self.model = model
+
+class QuestionResponse:
+    def __init__(self, sessionId, question, response):
+        self.sessId = sessionId
+        self.question = question
+        self.response = response
 
 class Event:
     def __init__(self, time, eventType):
@@ -24,20 +53,8 @@ class Click:
         self.hit = hit
         self.clicktophoton = clicktophoton
 
-# Helper function for convering lists of XYZ to azim/elev
-def toAzimElev(xyz):
-    """Helper for converting lists of XYZ coordinates to polar"""
-    positions = []
-    for [x,y,z] in xyz:
-        r = math.sqrt(x**2+y**2+z**2)
-        azim = math.atan(z/x)
-        elev = math.asin(y/r)
-        positions.append([r, azim, elev])
-    return positions
-
 class Importer:
     """Simple class for importing data from abstract-fps results files"""
-    IN_LOG_TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
     def __init__(self, dbName):
         self.db = sqlite3.connect(dbName)
@@ -88,15 +105,17 @@ class Importer:
 
     def getEvents(self):
         """Get all events from the events table"""
-        rows = self.getTableRows('event_log')
         events = []
-        for row in rows: events.append(Event(row[0], row[1]))
+        for row in self.getTableRows('event_log'): events.append(Event(row[0], row[1]))
         return events
 
-    def getTrialTargetPositionsXYZ(self, trial):
-        rows = self.queryDb("SELECT * FROM Target_Trajectory WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'")
-        positions = []
-        for row in rows: positions.append([row[1], row[2], row[3]])
+    def getTrialTargetPositionsXYZ(self, trial, targetId=None):
+        query = "SELECT * FROM Target_Trajectory WHERE [time] <= \'{0}\' AND [time] >= \'{1}\'".format(trial.endTime, trial.startTime)
+        if targetId is not None: query += ' AND [target_id] = \'{2}\''.format(targetId)
+        positions = {}
+        for row in self.queryDb(query): 
+            if row[1] not in positions.keys(): positions[row[1]] = [[row[2], row[3], row[4]]]
+            else: positions[row[1]].append([row[2], row[3], row[4]])
         return positions
 
     def getTargetPositionsXYZ(self, condId, trialIdx=0):
@@ -105,19 +124,33 @@ class Importer:
         if type(trial) is list: trial = trial[trialIdx]
         return self.getTrialTargetPositionsXYZ(trial)
 
+    # Helper function for convering lists of XYZ to azim/elev
+    def toAzimElev(self, target_xyz):
+        """Helper for converting lists of XYZ coordinates to polar"""
+        positions = {}
+        for targetId in target_xyz.keys():
+            positions[targetId] = []
+            for [x,y,z] in target_xyz[targetId]:
+                r = math.sqrt(x**2+y**2+z**2)
+                azim = math.atan(z/x)
+                elev = math.asin(y/r)
+                positions[targetId].append([r, azim, elev])
+        return positions
+
     def getTrialTargetPositionsAzimElev(self, trial):
         positionsXYZ = self.getTrialTargetPositionsXYZ(trial)
-        return toAzimElev(positionsXYZ)
+        return self.toAzimElev(positionsXYZ)
 
     def getTargetPositionsAzimElev(self, condId, trialIdx=0):
         """Get all target positions (for a given condition id) as azim/elev from Target_Trajectory table"""
         positionsXYZ = self.getTargetPositionsXYZ(condId, trialIdx)
-        return toAzimElev(positionsXYZ)
+        return self.toAzimElev(positionsXYZ)
     
     def getTrialPlayerActions(self, trial):
-        rows = self.queryDb("SELECT * FROM Player_Action WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'")
+        """Get all player actions from a particular trial"""
         actions = []
-        for row in rows: actions.append([row[2], row[3], row[1]])
+        for row in self.queryDb("SELECT * FROM Player_Action WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'"): 
+            actions.append(PlayerAction(row[0], row[1], row[2], row[3], row[4] , row[5], row[6], row[7]))
         return actions
 
     def getPlayerActions(self, condId, trialIdx=0):
@@ -128,12 +161,51 @@ class Importer:
 
     def getClicks(self):
         """Get click information from the database"""
-        actions = self.queryDb('SELECT * from Player_Action WHERE [event] = \'hit\' OR [event] = \'miss\'')
         clicks = []
-        for [t,event,azim,elev] in actions:
+        for [t,event,azim,elev] in self.queryDb('SELECT * from Player_Action WHERE [event] = \'hit\' OR [event] = \'miss\''):
             query = 'SELECT latency from click_latencies WHERE time >= \'{0}\' ORDER BY time ASC'.format(t)
             c2p = self.queryDb(query)
             if len(c2p) == 0: c2ptime = None
             else: c2ptime = c2p[0][0]
             clicks.append(Click(t, azim, elev, event == 'hit', c2ptime))
         return clicks
+
+    def getRowTarget(self, row):
+        """Get a target from a row returned from the database"""
+        # Dealing with a parametric target, get params
+        if(row[2] == 'parametrized'): 
+            params = {}
+            params["refresh_rate"] = row[3]
+            params["added_frame_lag"] = row[4]
+            params["min_ecc_h"] = row[5]
+            params["min_ecc_v"] = row[6]
+            params["max_ecc_h"] = row[7]
+            params["max_ecc_v"] = row[8]
+            params["min_speed"] = row[9]
+            params["max_speed"] = row[10]
+            params["min_motion_change_period"] = row[11]
+            params["max_motion_change_period"] = row[12]
+            params["jump_enabled"] = row[13]       
+        return Target(row[0], row[1], row[2], params, row[14])
+
+    def getTarget(self, targetId):
+        """Get a single target by its (unqiue) id"""
+        row = self.queryDb('SELECT * from Targets WHERE target_id = \'{0}\''.format(targetId))
+        if len(row) == 0: return None
+        return self.getRowTarget(row[0])
+
+    def getTrialTargets(self, trialId = None):
+        """Get a list of targets based on a trial ID"""
+        query = 'SELECT * from Targets '
+        if trialId is not None: query += 'WHERE trial_id = \'{0}\''.format(trialId)
+        rows = self.queryDb(query)
+        targets = []
+        for row in rows: targets.append(self.getRowTarget(row))
+
+    def getQuestionResponses(self, sessId = None):
+        query = 'SELECT * from Questions '
+        if sessId is not None: query += 'WHERE Session = \'{0}\''
+        rows = self.queryDb(query)
+        questions = []
+        for row in rows: questions.append(QuestionResponse(row[0], row[1], row[2]))
+        return questions
--- scripts/results analysis/aim_and_target.py
+++ scripts/results analysis/aim_and_target.py
@@ -3,8 +3,8 @@ from abstractfps_importer.Importer import Importer
 import sys
 import numpy as np
 
-AZIM_RANGE_DEG = 40         # Range of azimuths to plot
-ELEV_RANGE_DEG = 40         # Range of elevations to plot
+AZIM_RANGE_DEG = 360         # Range of azimuths to plot
+ELEV_RANGE_DEG = 360         # Range of elevations to plot
 
 if len(sys.argv) < 2: raise Exception("Provide db filename as input!")
 
@@ -15,42 +15,45 @@ trials = db.getTrials()
 # Analyze each trial in the results
 for trial in trials:
     # Get target trajectory and player actions from the db
-    trajectory = db.getTrialTargetPositionsAzimElev(trial)
+    trajectories = db.getTrialTargetPositionsAzimElev(trial)  # This is now a dictionary
     actions = db.getTrialPlayerActions(trial)
 
-    # Start by making a plot of the target trajectory
-    tazims = []; televs = []
-    for pt in trajectory:
-        tazims.append(pt[1]*180.0/np.pi)
-        televs.append(pt[2]*180.0/np.pi)
-    plt.scatter(tazims, televs, color='lightblue', s=5, marker='s')
+    # Start by making a plot of the target trajectory (per target)
+    for target in trajectories.keys():
+        trajectory = trajectories[target]
+        tazims = []; televs = []
+        for pt in trajectory:
+            tazims.append(pt[1]*180.0/np.pi)
+            televs.append(pt[2]*180.0/np.pi)
+        plt.scatter(tazims, televs, s=5, marker='s', label=target)
 
     # Now plot the player actions
     aim_azim = []; aim_elev = []; miss_azim = []; miss_elev = []; hit_azim = []; hit_elev = []
-    for pt in actions:
-        if(pt[2] == "aim"):
-            aim_azim.append(pt[0])
-            aim_elev.append(pt[1])
-        elif (pt[2] == "miss"):
-            miss_azim.append(pt[0])
-            miss_elev.append(pt[1])
+    for action in actions:
+        if(action.event == "aim"):
+            aim_azim.append(action.view_az)
+            aim_elev.append(action.view_el)
+        elif (action.event == "miss"):
+            miss_azim.append(action.view_az)
+            miss_elev.append(action.view_el)
         elif (pt[2] == "hit"):
-            hit_azim.append(pt[0])
-            hit_elev.append(pt[1])
+            hit_azim.append(action.view_az)
+            hit_elev.append(action.view_el)
 
     mean_azim = (np.mean(tazims) + np.mean(aim_azim))/2
     mean_elev = (np.mean(televs) + np.mean(aim_elev))/2
 
     # Update the plot
-    plt.scatter(aim_azim, aim_elev, color='lightgray',s=1)
-    plt.scatter(miss_azim, miss_elev, color='red', s=30, marker='x')
-    plt.scatter(hit_azim, hit_elev, color='green', s=30, marker='o')
+    plt.scatter(aim_azim, aim_elev, color='lightgray',s=1, label='Aim Trajectory')
+    plt.scatter(miss_azim, miss_elev, color='red', s=30, marker='x', label='Miss')
+    plt.scatter(hit_azim, hit_elev, color='green', s=30, marker='o', label='Hit')
 
     plt.title('Azimuth vs Elevation for Target Path and Player Action for Trial {0} #{1}'.format(trial.id, trial.idx+1))
     plt.xlabel('Azimuth (°)')
     plt.xlim(mean_azim-AZIM_RANGE_DEG/2, mean_azim+AZIM_RANGE_DEG/2)
     plt.ylabel('Elevation (°)')
     plt.ylim(mean_elev-ELEV_RANGE_DEG/2, mean_elev+ELEV_RANGE_DEG/2)
-    plt.legend(['Target Trajectory', 'Aim Trajectory', 'Miss', 'Hit'])
+    plt.legend()
+    plt.tight_layout()
 
     plt.show()
