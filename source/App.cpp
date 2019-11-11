954eb21ecb86e990c803b2615b433de953e0f433
Ben Boudaoud <bboudaoud@nvidia.com>
2019-11-11 17:06:08 -0500

    Minor fix for FPS printout location

--- source/App.cpp
+++ source/App.cpp
@@ -1751,8 +1751,7 @@ void App::onGraphics2D(RenderDevice* rd, Array<shared_ptr<Surface2D>>& posed2D) 
 			}
 
 			msg += format(" | %.1f min/%.1f avg/%.1f max ms", recentMin * 1000.0f, 1000.0f / renderDevice->stats().smoothFrameRate, 1000.0f * recentMax);
-
-			outputFont->draw2D(rd, msg, (Point2(30, 28) * scale).floor(), floor(20.0f * scale), Color3::yellow());
+			outputFont->draw2D(rd, msg, Point2(rd->viewport().width()*0.75, rd->viewport().height()*0.05).floor(), floor(20.0f*scale), Color3::yellow());
 		}
 
 		// Handle recording indicator
