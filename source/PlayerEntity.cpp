c3e527f556f99dc2d0ed83f1e086b8609e2ca181
joohwankimNV <sckim@nvidia.com>
2019-11-18 15:34:24 -0800

    Expanding m_headTilt range to -89.9 ~ 89.9 deg. Note that 89.9 is an arbitrary value close to 90 deg.

--- source/PlayerEntity.cpp
+++ source/PlayerEntity.cpp
@@ -138,7 +138,7 @@ void PlayerEntity::onSimulation(SimTime absoluteTime, SimTime deltaTime) {
 		m_inContact = slideMove(deltaTime);
 		m_headingRadians += m_desiredYawVelocity;	// *(float)deltaTime;		// Don't scale by time here
 		m_headingRadians = mod1(m_headingRadians / (2 * pif())) * 2 * pif();
-		m_headTilt = clamp(m_headTilt - m_desiredPitchVelocity, -80 * units::degrees(), 80 * units::degrees());
+		m_headTilt = clamp(m_headTilt - m_desiredPitchVelocity, -89.9f * units::degrees(), 89.9f * units::degrees());
 		m_frame.rotation = Matrix3::fromAxisAngle(Vector3::unitY(), -m_headingRadians) * Matrix3::fromAxisAngle(Vector3::unitX(), m_headTilt);
 
 		// Check for "off map" condition and reset position here...
