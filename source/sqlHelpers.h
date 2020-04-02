707e75e065346f3fe948d45c0117c85a18e212a2
Josef Spjut <jspjut@nvidia.com>
2020-04-02 13:13:30 -0400

    Removed unimplemented sqlHelper functions

--- source/sqlHelpers.h
+++ source/sqlHelpers.h
@@ -10,5 +10,3 @@
 void createTableInDB(sqlite3* db, String tableName, Array<Array<String>> columns);
 void insertRowIntoDB(sqlite3* db, String tableName, Array<String> values, String colNames = "");
 void insertRowsIntoDB(sqlite3* db, String tableName, Array<Array<String>> valueVector, String colNames = "");
-int getMaxID(sqlite3* db, String tableName);
-String addQuotes(String s);
