9f8f1b9b72d15bad5fe785629bda4f8ce3cfb8cd
Ben Boudaoud <bboudaoud@nvidia.com>
2020-07-17 11:00:53 -0400

    Extend model scaling range w/ scale factor

--- source/TargetEntity.h
+++ source/TargetEntity.h
@@ -8,7 +8,7 @@ class TargetConfig;
 #define BOUNDING_SPHERE_RADIUS	0.5		///< Use a 0.5m radius for sizing here
 
 // Scale and offset for target
-const float TARGET_MODEL_ARRAY_SCALING = 0.2f;
+const float TARGET_MODEL_ARRAY_SCALING = 0.4f;
 const float TARGET_MODEL_ARRAY_OFFSET = 20;
 
 struct Destination{
