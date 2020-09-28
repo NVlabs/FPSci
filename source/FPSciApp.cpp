4742a5a18e9eef73c0a63976642c7cb0ed9a2b45
Josef Spjut <jspjut@nvidia.com>
2020-09-28 08:58:18 -0400

    Adjust order of cmp360 button

--- source/FPSciApp.cpp
+++ source/FPSciApp.cpp
@@ -119,7 +119,7 @@ void FPSciApp::updateMouseSensitivity() {
 	const shared_ptr<UserConfig> user = currentUser();
 	// Converting from mouseDPI (dots/in) and sensitivity (cm/turn) into rad/dot which explains cm->in (2.54) and turn->rad (2*PI) factors
 	// rad/dot = rad/cm * cm/dot = 2PI / (cm/turn) * 2.54 / (dots/in) = (2.54 * 2PI)/ (DPI * cm/360)
-	const double cmp360 = 36 / user->mouseDegPerMm;
+	const double cmp360 = 36.0 / user->mouseDegPerMm;
 	const double radiansPerDot = 2.0 * pi() * 2.54 / (cmp360 * user->mouseDPI);
 	const shared_ptr<FirstPersonManipulator>& fpm = dynamic_pointer_cast<FirstPersonManipulator>(cameraManipulator());
 
--- source/GuiElements.cpp
+++ source/GuiElements.cpp
@@ -422,16 +422,18 @@ void UserMenu::drawUserPane(const MenuConfig& config, UserConfig& user)
 		m_currentUserPane->addLabel(format("Mouse DPI: %f", user.mouseDPI));
 	} m_currentUserPane->endRow();
 	m_currentUserPane->beginRow(); {
-		auto sensitivityNb = m_currentUserPane->addNumberBox("Mouse Sens", &(user.mouseDegPerMm), "°/mm", GuiTheme::LOG_SLIDER, 0.1, 60.0, 0.01);
+		auto sensitivityNb = m_currentUserPane->addNumberBox("Mouse Sens", &(user.mouseDegPerMm), "°/mm", GuiTheme::LOG_SLIDER, 0.01, 60.0, 0.01);
 		sensitivityNb->setWidth(320.0);
 		sensitivityNb->setUnitsSize(40.0);
 		sensitivityNb->setEnabled(config.allowSensitivityChange);
 	} m_currentUserPane->endRow();
 	m_currentUserPane->beginRow(); {
-		auto b = m_currentUserPane->addButton("Get cm/360°", this, &UserMenu::updateCmp360Press);
-		b->setEnabled(config.allowSensitivityChange);
-		m_cmp360 = m_currentUserPane->addLabel(format("%.1f cm/360°", 36 / user.mouseDegPerMm));
+		m_cmp360 = m_currentUserPane->addLabel(format("%.2f cm/360°", 36.0 / user.mouseDegPerMm));
+		m_cmp360->moveBy(Vector2(80., 0.));
 		m_cmp360->setEnabled(config.allowSensitivityChange);
+		auto b = m_currentUserPane->addButton("Update cm/360°", this, &UserMenu::updateCmp360Press);
+		b->setEnabled(config.allowSensitivityChange);
+		b->moveBy(Vector2(-120., 0.));
 	} m_currentUserPane->endRow();
 
 	if (config.allowTurnScaleChange) {
@@ -651,7 +653,7 @@ void UserMenu::updateSessionPress() {
 
 void UserMenu::updateCmp360Press() {
 	const UserConfig user = m_users.users[m_users.getUserIndex(m_userStatus.currentUser)];
-	m_cmp360->setCaption(format("%.1f cm/360°", 36/user.mouseDegPerMm));
+	m_cmp360->setCaption(format("%.2f cm/360°", 36.0/user.mouseDegPerMm));
 }
 
 void UserMenu::setVisible(bool enable) {
